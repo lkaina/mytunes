1bbf20b246ecc72760a6aade89030f56792fba03
Leighton <leighton.kaina@gmail.com>
2013-10-09 21:00:02 -0700

    passed first 2 tests - playFirst working on 'add' event.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,22 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    // this.bind('add', this.playFirst, this);
+    this.on('add', function(song){
+      if(this.length === 1){
+        this.playFirst();
+      }
+    }, this);
+
+    this.on('', function(song){
+      debugger;
+      this.remove(song);
+    });
+
+  },
+
+  playFirst: function(){
+    this.models[0].play();
   }
 
 });
